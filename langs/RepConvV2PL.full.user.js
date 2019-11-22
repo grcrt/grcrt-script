13388294288d03cc205ed4acf5304d3bcf85474e
Potusek <potusek@grcrt.net>
2019-11-22 23:12:33 +0100

    feat(mousescroll): added map zooming on mouse scroll Closes #40

--- langs/RepConvV2PL.full.user.js
+++ langs/RepConvV2PL.full.user.js
@@ -231,5 +231,6 @@ RepConvLangArray.pl = {
     BTNCOMPARE : 'Pakt vs Wróg',
     ALLYCOMPARETITLE: 'Porównanie sojuszy koalicji z wrogimi sojuszami',
     CHKMCOL : 'Przypisz kolory wiadomościom zgodnie z ustalonym schematem kolorowania',
-    CHKBUPO : 'Pokaż punkty rozbudowy'
+    CHKBUPO : 'Pokaż punkty rozbudowy',
+    CHKMOUSEWHEELZOOMBULLEYE: 'Użyj kółka myszy do zmiany przybliżenia mapy'
 }
